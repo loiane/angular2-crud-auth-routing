dd5d95479160cbbcdba33da20afd1de875fbd605
loiane <loianeg@gmail.com>
2016-12-14 15:50:29 -0200

    changed labels to English

--- src/app/shared/home.component.ts
+++ src/app/shared/home.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   template: `
-    <h3>Bem vindo!</h3>
+    <h3>Welcome!</h3>
   `
 })
 export class HomeComponent {}
--- src/app/shared/not-found.component.ts
+++ src/app/shared/not-found.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-not-found',
   template: `
-    <h1>Não encontrado!</h1>
+    <h1>Not found!</h1>
   `
 })
 export class NotFoundComponent {}
